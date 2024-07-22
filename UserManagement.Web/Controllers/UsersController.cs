b040b64ffb29c3c812850c279b2c34ad15ed5193
Ishmael Shariff <ishariff@spartaglobal.com>
2024-07-22 18:32:32 +0100

    Filters Section (Standard)

--- UserManagement.Web/Controllers/UsersController.cs
+++ UserManagement.Web/Controllers/UsersController.cs
@@ -1,20 +1,19 @@
-﻿using System.Linq;
+﻿using System;
+using System.Linq;
+using UserManagement.Models;
 using UserManagement.Services.Domain.Interfaces;
 using UserManagement.Web.Models.Users;
 
 namespace UserManagement.WebMS.Controllers;
 
-[Route("users")]
+[Route("users/")]
 public class UsersController : Controller
 {
     private readonly IUserService _userService;
     public UsersController(IUserService userService) => _userService = userService;
 
-    [HttpGet]
-    public ViewResult List()
-    {
-        var items = _userService.GetAll().Select(p => new UserListItemViewModel
-        {
+    private UserListViewModel createUserListViewModel(IEnumerable<User> users){
+        var userListItemViewModel = users.Select(p => new UserListItemViewModel{
             Id = p.Id,
             Forename = p.Forename,
             Surname = p.Surname,
@@ -22,11 +21,23 @@ public class UsersController : Controller
             IsActive = p.IsActive
         });
 
-        var model = new UserListViewModel
-        {
-            Items = items.ToList()
+        var userListViewModel = new UserListViewModel{
+            Items = userListItemViewModel.ToList()
         };
+        return userListViewModel;
+    }
 
-        return View(model);
+    [HttpGet]
+    [Route ("list")]
+    public ViewResult List(){
+        return View(createUserListViewModel(_userService.GetAll()));
+    }
+    
+    [HttpGet]
+    [Route ("list/filter/isActive")]
+    [ActionName("IsActive")]
+    public ViewResult List(string value){
+         bool isActive = Boolean.Parse(value);
+        return View("List", createUserListViewModel(_userService.FilterByActive(isActive)));
     }
 }
